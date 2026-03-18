2a3d9086d1260964936319f5a7dddabbc1e4af76
Joao Dutra <joao.dutra@capgemini.com>
2026-03-18 14:18:24 -0300

    26: creating home page form

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -19,7 +19,26 @@ export function App() {
 
             <Container>
                 <Countdown />
-            </Container>            
+            </Container>
+
+            <Container>
+                <form action="" className="form">
+                    <div className="formRow">
+                        <label htmlFor='input'>Tarefa</label>
+                        <input type="text" id="input" />
+                    </div>
+                    <div className="formRow">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="formRow">
+                        <p>Ciclos</p>
+                        <p>O O O O O O O</p>
+                    </div>
+                    <div className="formRow">
+                        <button>Enviar</button>
+                    </div>
+                </form>
+            </Container>    
         </>
     );
 }
