3988e78a54d66888fe244124d8f34c80d5046ba6
Joao Dutra <joao.dutra@capgemini.com>
2026-03-18 09:50:21 -0300

    initial commit for chronos project

---
+++ chronos-pomodoro/src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/theme.css';
+import './styles/global.css';
+
+export function App() {
+    return (
+        <>
+            <h1>Olá, mundo!</h1>
+            <p>Lorem ipsum dolor sit blablablablablablabalbalabalba</p>
+        </>
+    );
+}
