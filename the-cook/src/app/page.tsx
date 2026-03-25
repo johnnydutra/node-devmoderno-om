817e7e5d53160d51c48a151263fd2af7a11278be
Johnny Dutra <johnny.dutra@gmail.com>
2026-03-25 16:35:28 -0300

    105: basics on tailwind css

--- the-cook/src/app/page.tsx
+++ the-cook/src/app/page.tsx
@@ -1,3 +1,15 @@
+import clsx from "clsx";
+
 export default function HomePage() {
-  return <h1>Hello!</h1>;
+  return (
+    <div>
+      <h1
+        className={clsx(
+          "text-xl, font-bold, text-blue, transition duration-300",
+        )}
+      >
+        texto de teste
+      </h1>
+    </div>
+  );
 }
