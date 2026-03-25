8c63dc468a7bbc0276b0874c12539df9fd325c4d
Johnny Dutra <johnny.dutra@gmail.com>
2026-03-25 16:53:29 -0300

    106: server vs client components

--- the-cook/src/app/page.tsx
+++ the-cook/src/app/page.tsx
@@ -1,15 +1,10 @@
+import { Header } from "@/components/Header";
 import clsx from "clsx";
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
     <div>
-      <h1
-        className={clsx(
-          "text-xl, font-bold, text-blue, transition duration-300",
-        )}
-      >
-        texto de teste
-      </h1>
+      <Header />
     </div>
   );
 }
