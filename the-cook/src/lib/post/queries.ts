66aa78a31fa0ff95bf0f5fd46f1ee69ea99a7684
Johnny Dutra <johnny.dutra@gmail.com>
2026-03-27 15:11:40 -0300

    132: refining findPostBySlugCached

--- the-cook/src/lib/post/queries.ts
+++ the-cook/src/lib/post/queries.ts
@@ -1,8 +1,15 @@
 import { postRepository } from '@/repositories/post';
+import { notFound } from 'next/navigation';
 import { cache } from 'react';
 
 export const findAllPublishedPostsCached = cache(async () => await postRepository.findAllPublished());
 
-export const findPostBySlugCached = cache(async (slug: string) => await postRepository.findBySlug(slug));
+export const findPostBySlugCached = cache(async (slug: string) => {
+  const post = postRepository.findBySlug(slug).catch(() => undefined);
+
+  if (!post) notFound();
+
+  return post;
+});
 
 export const findPostByIdCached = cache(async (id: string) => await postRepository.findById(id));
