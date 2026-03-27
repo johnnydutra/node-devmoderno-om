e3624c4e2626414d620e424cc17de5ed4f06f25e
Johnny Dutra <johnny.dutra@gmail.com>
2026-03-27 11:05:14 -0300

    127: using layout

--- the-cook/src/app/layout.tsx
+++ the-cook/src/app/layout.tsx
@@ -1,9 +1,15 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
+import { Container } from '@/components/Container';
+import { Header } from '@/components/Header';
+import clsx from 'clsx';
 
 export const metadata: Metadata = {
-  title: "Próximo Prato",
-  description: "Projeto do curso de NextJS 15 com React 19 e Tailwind 4",
+  title: {
+    default: 'Próximo Prato',
+    template: '%s | Próximo Prato',
+  },
+  description: 'Projeto do curso de NextJS 15 com React 19 e Tailwind 4',
 };
 
 export default function RootLayout({
@@ -12,8 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body>
+        <Container>
+          <Header />
+          <main>{children}</main>
+
+          <footer>
+            <p className={clsx('text-6xl', 'font-bold', 'text-center', 'py-8')}>FOOTER</p>
+          </footer>
+        </Container>
+      </body>
     </html>
   );
 }
--- the-cook/src/app/page.tsx
+++ the-cook/src/app/page.tsx
@@ -1,16 +1,11 @@
-import { Container } from '@/components/Container';
-import { Header } from '@/components/Header';
 import { PostFeatured } from '@/components/PostFeatured';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
-import clsx from 'clsx';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostFeatured />
       </Suspense>
@@ -18,10 +13,6 @@ export default async function HomePage() {
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-
-      <footer>
-        <p className={clsx('text-6xl', 'font-bold', 'text-center', 'py-8')}>FOOTER</p>
-      </footer>
-    </Container>
+    </>
   );
 }
