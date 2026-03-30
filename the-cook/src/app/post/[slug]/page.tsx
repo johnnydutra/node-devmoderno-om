76fd08e2ad9f21d8cb8cea1359411b83f1cec392
Johnny Dutra <johnny.dutra@gmail.com>
2026-03-30 08:19:25 -0300

    134: using Suspense in slug page

--- the-cook/src/app/post/[slug]/page.tsx
+++ the-cook/src/app/post/[slug]/page.tsx
@@ -1,5 +1,8 @@
+import { SinglePost } from '@/components/SinglePost';
+import { SpinLoader } from '@/components/SpinLoader';
 import { findPostBySlugCached } from '@/lib/post/queries';
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
@@ -17,11 +20,10 @@ export async function generateMetadata({ params }: PostSlugPageProps): Promise<M
 
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
-  const post = await findPostBySlugCached(slug);
 
   return (
-    <div>
-      <p>{post.title}</p>
-    </div>
+    <Suspense fallback={<SpinLoader containerClassesToAdd='min-h-20 mb-16' />}>
+      <SinglePost slug={slug} />
+    </Suspense>
   );
 }
