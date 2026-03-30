a0e586c6448db7e7a4c788f2f5882576e623781e
Johnny Dutra <johnny.dutra@gmail.com>
2026-03-30 08:00:09 -0300

    133: generateMetadata for dynamic route

--- the-cook/src/app/not-found.tsx
+++ the-cook/src/app/not-found.tsx
@@ -2,15 +2,18 @@ import clsx from 'clsx';
 
 export default function NotFoundPage() {
   return (
-    <div
-      className={clsx(
-        'min-h-320px bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center',
-      )}
-    >
-      <div>
-        <h1 className='text-7xl/tight mb-4 font-extrabold'>Ops!</h1>
-        <p>A página que você tentou acessar não existe.</p>
+    <>
+      <title>Página não encontrada</title>
+      <div
+        className={clsx(
+          'min-h-320px bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center',
+        )}
+      >
+        <div>
+          <h1 className='text-7xl/tight mb-4 font-extrabold'>Ops!</h1>
+          <p>A página que você tentou acessar não existe.</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
