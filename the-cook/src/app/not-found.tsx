e5f8418074a2e09cf359f98766338b2db9ee9c20
Johnny Dutra <johnny.dutra@gmail.com>
2026-03-31 12:08:46 -0300

    139: ErrorMessage component creation

--- the-cook/src/app/not-found.tsx
+++ the-cook/src/app/not-found.tsx
@@ -1,19 +1,11 @@
-import clsx from 'clsx';
+import ErrorMessage from '@/components/ErrorMessage';
 
 export default function NotFoundPage() {
   return (
-    <>
-      <title>Página não encontrada</title>
-      <div
-        className={clsx(
-          'min-h-320px bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center',
-        )}
-      >
-        <div>
-          <h1 className='text-7xl/tight mb-4 font-extrabold'>Ops!</h1>
-          <p>A página que você tentou acessar não existe.</p>
-        </div>
-      </div>
-    </>
+    <ErrorMessage
+      pageTitle='Página não encontrada'
+      contentTitle='Ops!'
+      content='A página que você tentou acessar não existe'
+    />
   );
 }
