ea7657ea8927dc735b480b0b19d65292fe69cb56
Johnny Dutra <johnny.dutra@gmail.com>
2026-03-31 19:24:13 -0300

    148: drizzle and migrations

--- the-cook/drizzle.config.ts
+++ the-cook/drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'drizzle-kit';
 import { resolve } from 'path';
 
 export default defineConfig({
-  out: resolve(process.cwd(), 'src', 'db', 'drizzle', 'migrations'),
-  schema: resolve(process.cwd(), 'src', 'db', 'drizzle', 'schemas.ts'),
+  out: './src/db/drizzle/migrations',
+  schema: './src/db/drizzle/schemas.ts',
   dialect: 'sqlite',
   dbCredentials: {
     url: resolve(process.cwd(), 'db.sqlite3'),
